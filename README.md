3dc98fcfa67ae7d725918161369c09bcca6d9783
Dina-72 <amenova1109@yandex.ru>
2023-08-20 23:54:10 +0500

    error commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/2mp2ia3rmt64wavg?svg=true)](https://ci.appveyor.com/project/Dina1109/webselenide)
---
+++ src/test/java/ru/netology/webselenide/ApplicationCardDeliveryTest.java
@@ -0,0 +1,35 @@
+package ru.netology.webselenide;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class ApplicationCardDeliveryTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldSuccessfulForm() {
+        Selenide.open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Самара");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='name'] input").setValue("Петров Петр");
+        $("[data-test-id='phone'] input").setValue("+71231231234");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+    }
+
+}
